8d5af3f16542d368a59b3563421b30ff9c5da795
Vadim-Cr <vadim.crudu@gmail.com>
2023-05-04 17:56:11 +0200

    still at css classes

---
+++ logic.js
@@ -0,0 +1,7 @@
+// definisco il mio array di immagini
+
+
+const imgCarousel = ["consegna/img/01.webp", "consegna/img/02.webp", "consegna/img/03.webp", "consegna/img/04.webp", "consegna/img/05.webp"]
+
+const itemsContainer = document.getElementById("items_container");
+console.log(itemsContainer);
