baee08182e8d9709de7882fa0082e9c261851473
Vadim-Cr <vadim.crudu@gmail.com>
2023-05-05 01:01:56 +0200

    finish, no bonus

--- logic.js
+++ logic.js
@@ -1,7 +1,36 @@
 // definisco il mio array di immagini
+const imgCarousel = ["consegna/img/01.webp", "consegna/img/02.webp", "consegna/img/03.webp", "consegna/img/04.webp", "consegna/img/05.webp"];
+
+// devo prendere un immagine dall'array e inserirla dentro ad un div con le opportune classi in modo che ad ogni click esca l'immagine
+let currentIndex = 0;
+let prevActiveImg = null;
+
+// prendo la freccia per next
+const arrowNext = document.getElementById("next");
+
+// collego la freccia Next all'evento click
+arrowNext.addEventListener("click", function () {
+  if (currentIndex < imgCarousel.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+
+    //levo la classe "active"
+
+  if (prevActiveImg) {
+    prevActiveImg.classList.remove("active");
+    prevActiveImg.classList.add("invisible");
+  }
+
+  let imgItem = `<div class="my_img"><img src="${imgCarousel[currentIndex]}" alt="image"></div>`;
+  const itemsContainer = document.getElementById("items_container");
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = imgItem;
+  itemsContainer.append(newDiv);
+
+// memorizzo l'immagine attuale come quella precedente
+prevActiveImg = newDiv.querySelector(".my_img");
 
 
-const imgCarousel = ["consegna/img/01.webp", "consegna/img/02.webp", "consegna/img/03.webp", "consegna/img/04.webp", "consegna/img/05.webp"]
-
-const itemsContainer = document.getElementById("items_container");
-console.log(itemsContainer);
+});
